eaf57deee6add29cccdcb69649164d187102c062
Daffa Nashwan Pramono <112962100+daffnp1012@users.noreply.github.com>
2023-10-27 21:01:34 +0700

    forgot to add the title...

--- app/src/main/java/com/dnpstudio/dnphaditsapp/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/dnpstudio/dnphaditsapp/ui/splash/SplashScreen.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.dnpstudio.dnphaditsapp.navigation.ScreenRoute
@@ -29,6 +31,9 @@ fun SplashScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-
+        Text(
+            text = "DnpHaditsApp",
+            fontSize = 28.sp
+        )
     }
 }
