89d6a2a2a587f23b94e1ff80329a580aef32c43a
Daffa Nashwan Pramono <112962100+daffnp1012@users.noreply.github.com>
2023-10-27 20:55:43 +0700

    remove DetailScreen.kt

--- app/src/main/java/com/dnpstudio/dnphaditsapp/navigation/Router.kt
+++ app/src/main/java/com/dnpstudio/dnphaditsapp/navigation/Router.kt
@@ -3,5 +3,4 @@ package com.dnpstudio.dnphaditsapp.navigation
 sealed class ScreenRoute(val route: String){
     object splash: ScreenRoute("splash")
     object home: ScreenRoute("home")
-    object detail: ScreenRoute("detail")
 }
